d83c9e5e48443352164e8c8acbbbaf8751d86773
Vladimir Janjic <vj32@st-andrews.ac.uk>
2015-06-26 12:37:47 +0100

    changes to the gpu code for APSP

--- APSP/sssp_gpu.erl
+++ APSP/sssp_gpu.erl
@@ -4,14 +4,33 @@
 
 program(ok) -> "
 __kernel void nbody_kern(
-                         __global uint *dist_mat,
-                         __global uint *indices
+                         __global int *dist_mat,
+                         __global uint *indices,
+                                  uint nr_nodes,
                          __global uint *out
+                         __global uint *marked
                         )
 {
-  int gtid = get_global_id(0);
-  int my_ind = out[gtid];
+  uint gtid = get_global_id(0);
+  uint my_ind = out[gtid];
+  uint i, j, min_dist = (uint) 4294967296, min_ind;
+
+  for (i=0; i<nr_nodes; i++) {
+    if (i!=my_ind) 
+      out[i] = 4294967296;
+    else 
+      out[i] = 0;
+  marked[i] = 0;
   
+  for (i=0; i<nr_nodes; i++) {
+    for (j=0; j<nr_nodes; j++) 
+      if (!marked[j] && out[j] < min_dist) {
+        min_dst = out[j];
+        min_ind = j;
+      }
+    
+  }
+
 }
 
 ".
